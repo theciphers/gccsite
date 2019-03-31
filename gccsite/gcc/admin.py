0466c88ce945dc7ed1977cd9c6abdd1dca632847
Rémi Dupré <remim.dupre@gmail.com>
2019-03-31 21:00:50 +0200

    customize django administration

--- gccsite/gcc/admin.py
+++ gccsite/gcc/admin.py
@@ -3,24 +3,39 @@ from django.db.models import Q
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 
-from gcc.models import (Answer, Applicant, ApplicantLabel, ApplicantStatusTypes,
-                        Corrector, Edition, Event, EventWish, Form, Question,
-                        SubscriberEmail, Sponsor)
+import gcc.models as models
 
 
+admin.site.register([models.ApplicantLabel, models.Edition])
 
-admin.site.register([ApplicantLabel, Edition, SubscriberEmail, Question])
+
+# -- Forms
 
 class QuestionInline(SortableTabularInline):
-    model = Form.question_list.through
+    model = models.Form.question_list.through
     extra = 1
 
-@admin.register(Form)
+@admin.register(models.Form)
 class FormAdmin(NonSortableParentAdmin):
     inlines = [QuestionInline]
 
+
+# -- Applicant
+
 class EventWishesInline(admin.TabularInline):
-    model = EventWish
+    model = models.EventWish
+    max_num = 3
+
+
+class AnswersInline(admin.TabularInline):
+    def answer(self, obj):
+        return str(obj)
+
+    model = models.Answer
+    fields = readonly_fields = ['question', 'answer']
+    can_delete = False
+    extra = 0
+
 
 class ApplicationStatusFilter(admin.SimpleListFilter):
     title = _('status')
@@ -28,61 +43,78 @@ class ApplicationStatusFilter(admin.SimpleListFilter):
 
     def lookups(self, request, model_admin):
         return ((str(item.value), name)
-                for name, item in ApplicantStatusTypes.__members__.items())
+                for name, item in models.ApplicantStatusTypes.__members__.items())
 
     def queryset(self, request, queryset):
         if self.value() is None:
             return queryset
 
-        if int(self.value()) == ApplicantStatusTypes.incomplete.value:
-            return queryset.filter(Q(eventwish__status=0) | Q(eventwish=None))
+        if int(self.value()) == models.ApplicantStatusTypes.incomplete.value:
+            return queryset.filter(
+                Q(eventwish__status=0) | Q(eventwish=None)).distinct()
 
         return queryset.filter(eventwish__status=0)
 
-@admin.register(Applicant)
+
+@admin.register(models.Applicant)
 class ApplicationAdmin(admin.ModelAdmin):
-    Applicant.get_status_display.short_description = _('status')
+    models.Applicant.get_status_display.short_description = _('status')
 
-    search_fields = ('user__username', 'user__first_name', 'user__last_name', 'user__email')
-    list_display = ('user', 'edition', 'get_status_display')
-    list_filter = ('edition', ApplicationStatusFilter, 'assignation_wishes__center')
-    readonly_fields = ('user', 'edition')
-    fieldsets = (
-        (None, {'fields': ('user', 'edition', )}),
-        (_('Review'), {'fields': ('labels', )}))
-    inlines = [EventWishesInline]
-
-
-@admin.register(Corrector)
-class CorrectorAdmin(admin.ModelAdmin):
-    list_display = ('user', 'event')
+    search_fields = ['user__username', 'user__first_name', 'user__last_name',
+                     'user__email']
+    list_display = ['user', 'edition', 'get_status_display']
+    list_filter = ['edition', ApplicationStatusFilter, 'assignation_wishes__center']
+    readonly_fields = ['user', 'edition']
+    fieldsets = [(None, {'fields': ['user', 'edition']}),
+                 (_('Review'), {'fields': ['labels']})]
+    inlines = (EventWishesInline, AnswersInline)
 
 
-@admin.register(Event)
+# -- Event
+
+class CorrectorInline(admin.TabularInline):
+    model = models.Corrector
+
+
+@admin.register(models.Event)
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('edition', 'center', 'event_start', 'event_end',
-                    'signup_start', 'signup_end')
+    list_display = ['edition', 'center', 'event_start', 'event_end',
+                    'signup_start', 'signup_end']
+    list_filter = ['edition', 'center']
+    inlines = [CorrectorInline]
 
 
-@admin.register(Answer)
-class AnswerAdmin(admin.ModelAdmin):
-    list_display = ('applicant', 'question', '__str__')
+# -- Question
+
+@admin.register(models.Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['question', 'comment', 'response_type']
+    search_fields = ['question', 'comment']
 
 
-@admin.register(Sponsor)
+# -- Sponsor
+
+@admin.register(models.Sponsor)
 class SponsorAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'site', 'contact_position',
-                    'contact_first_name', 'contact_last_name',
-                    'is_active_bool',)
-    list_filter = ('is_active',)
-    ordering = ('name',)
-    search_fields = ('name', 'description', 'comment', 'site',
+    list_display = ['name', 'description', 'site', 'contact_position',
+                    'contact_first_name', 'contact_last_name', 'is_active_bool']
+    list_filter = ['is_active']
+    ordering = ['name']
+    search_fields = ['name', 'description', 'comment', 'site',
                      'contact_position', 'contact_email', 'contact_first_name',
                      'contact_last_name', 'contact_phone_desk',
-                     'contact_phone_mobile', 'contact_phone_fax',)
+                     'contact_phone_mobile', 'contact_phone_fax']
 
     def is_active_bool(self, obj):
         return obj.is_active
+
     is_active_bool.admin_order_field = 'is_active'
     is_active_bool.short_description = _("Active")
     is_active_bool.boolean = True
+
+
+# Newsletter
+
+@admin.register(models.SubscriberEmail)
+class SubscriberEmailAdmin(admin.ModelAdmin):
+    search_fields = ['email']
