c0628451f7c7ec545a235ddc86fd654c353be0c4
Rémi Dupré <remim.dupre@gmail.com>
2019-05-28 13:59:07 +0200

    templatetags: add missing files for application_status

---
+++ gccsite/gcc/templatetags/application_status.py
@@ -0,0 +1,22 @@
+from django import template
+from gcc.models import ApplicantStatusTypes as StatusType
+
+
+register = template.Library()
+
+
+def status_badge(status):
+    """
+    Get the bootstrap's badge class name that best represents the input status.
+    """
+    if status in [StatusType.pending.value]:
+        return 'badge-default'
+    if status in [StatusType.selected.value, StatusType.confirmed.value]:
+        return 'badge-success'
+    if status in [StatusType.incomplete.value, StatusType.accepted.value]:
+        return 'badge-warning'
+    if status in [StatusType.rejected.value]:
+        return 'badge-danger'
+
+
+register.filter('status_badge', status_badge)
